b10b0217e695ab937bf60b2a235eaeb664aa7209
Antonio Luna <felipec883@outlook.com>
2023-09-01 22:14:30 -0400

    Add B

---
+++ b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)  {
+ 
+
+//BRISBIANO GOL CODE
+
+printf("HELLO BOBI");
+printf("baroly gol");
+
+//ADDED BAROLY GOL
+//ADDED HELLO BOBI CODE NWE WORLD
+
+return 0;
+
+}
